2a33e75ff78f1db0215b66ee0af27fed60885bbb
Всеволод <63463168+vsecoder@users.noreply.github.com>
2022-06-04 13:06:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ let tg = window.Telegram.WebApp;
 let data = tg.initDataUnsafe;
 
 document.querySelector('#title').innerText = `Hi, ${data.user.first_name}!`;
-document.querySelector('#paragraf').innerText = `.config -> bio_url -> Enter value`;
+document.querySelector('#paragraf').innerText = `Random text)))`;
